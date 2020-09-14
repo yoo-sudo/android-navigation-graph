ede5ab035f1bd1fdbea2c46431a2ad063ffff4f7
yogeshwaran <yogwaran5@gmail.com>
2020-09-14 20:10:00 +0530

    fragment navigation using nav graph

---
+++ app/src/main/java/com/example/navgraph/FirstFragment.kt
@@ -0,0 +1,33 @@
+package com.example.navgraph
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_first, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nav = Navigation.findNavController(activity!!, R.id.nav_host_fragment);
+
+        tvNext.setOnClickListener {
+            Bundle().apply {
+                putString("test", "helo")
+                nav.navigate(R.id.action_firstFragment_to_secondFragment, this)
+//                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment, this)
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/navgraph/SecondFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navigationgraph
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.navgraph.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_second, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            //to pass arguments from nav graph
+            /*Toast.makeText(
+                activity,
+                "navigation argument" + FirstFragmentArgs.fromBundle(arguments!!).testBool,
+                Toast.LENGTH_SHORT
+            ).show()*/
+
+            tvText.text = arguments!!.getString("test")
+        }
+
+        tvBack.setOnClickListener {
+            Navigation.findNavController(view).navigateUp()
+        }
+
+
+    }
+}
